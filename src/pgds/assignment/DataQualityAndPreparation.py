f89763f35d2b4d5247114ecf8d1ab518480f6b75
awsbiplab <mitra.kumarbiplab@gmail.com>
2026-04-05 11:09:26 +0530

    data set commit

---
+++ src/pgds/assignment/DataQualityAndPreparation.py
@@ -0,0 +1,13 @@
+"""
+DataPreparation handles cleaning and preprocessing of loan datasets.
+
+Responsibilities:
+- Validate and clean datasets
+- Handle missing values and duplicates
+- Standardize date formats
+- Remove or cap outliers in numeric columns
+Attributes:
+    None
+Methods:
+    clean_data(df): Cleans and preprocesses the dataset
+"""
